b9d3aa670cdb22e28c87689b4e12428b8a68bd5e
Andrey Helldar <helldar@ai-rus.com>
2017-02-20 23:58:50 +0900

    Add new status messages

--- src/lang/en/api.php
+++ src/lang/en/api.php
@@ -5,7 +5,14 @@
  * @version 2017-02-20
  */
 
-return array(
+return [
     0 => 'Other error',
     1 => 'Unknown method',
-);
+    2 => 'Unknown error!',
+
+    10 => 'Access denied.',
+    11 => 'Unauthorized.',
+
+    20 => 'Successfully.',
+    21 => 'Error.',
+];
