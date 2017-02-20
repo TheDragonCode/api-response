3634e07ff2d5c044935b7b8e79e5df379ead8646
Andrey Helldar <helldar@ai-rus.com>
2017-02-20 23:07:30 +0900

    Add fake test (Temporarily)

--- tests/ApiResponseTest.php
+++ tests/ApiResponseTest.php
@@ -1,50 +1,14 @@
 <?php
+
 /**
  * @author  Andrey Helldar <helldar@ai-rus.com>
  * @version 2017-02-20
  */
-
-
-namespace Helldar\ApiResponse;
-
-
-class ApiResponseTest extends \PHPUnit_Framework_TestCase
+class ApiResponseTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var ApiResponse
-     */
-    protected $object;
-
-    public function testResponse()
+    public function testApiResponse()
     {
-        $this->assertEquals(json_encode([
-            'status_code' => 0,
-            'status_text' => 'Other error',
-            'response'    => null,
-        ]), $this->object->response(0, null, 200));
-
-        $this->assertEquals(json_encode([
-            'status_code' => 1,
-            'status_text' => 'Unknown method',
-            'response'    => 'Unknown method',
-        ]), $this->object->response(1, null, 200));
+        $this->assertTrue(true);
     }
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        $this->object = new ApiResponse();
-    }
-
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-        // none
-    }
 }
