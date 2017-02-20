d29c6d6c8ea9f2e90ba8aa85c6856fedd0e09bfc
Andrey Helldar <helldar@ai-rus.com>
2017-02-20 14:51:47 +0000

    Apply fixes from StyleCI

--- src/lang/en/api.php
+++ src/lang/en/api.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * @author  Andrey Helldar <helldar@ai-rus.com>
+ *
  * @version 2017-02-20
  */
 
-return [
+return array(
     0 => 'Other error',
     1 => 'Unknown method',
-];
+);
