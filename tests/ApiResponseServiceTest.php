44b9370bee22902006ffe3f0d062b77b0232640b
Andrey Helldar <helldar@ai-rus.com>
2018-06-18 11:09:31 +0300

    Added tests

--- tests/ApiResponseServiceTest.php
+++ tests/ApiResponseServiceTest.php
@@ -2,15 +2,23 @@
 
 namespace Helldar\ApiResponse\Tests;
 
-use Helldar\ApiResponse\Services\ApiResponseService;
 use PHPUnit\Framework\TestCase;
 
 class ApiResponseServiceTest extends TestCase
 {
     public function testInit()
     {
-        $instance = ApiResponseService::init();
+        $this->assertJson(api_response('ok')->getContent());
+        $this->assertJson(api_response('fail', 400)->getContent());
 
-        $this->assertEquals($instance instanceof ApiResponseService, true);
+        $this->assertJsonStringEqualsJsonString(api_response('ok')->getContent(), json_encode('ok'));
+        $this->assertEquals(api_response('ok')->getContent(), json_encode('ok'));
+
+        $this->assertEquals(api_response('ok', 401)->getStatusCode(), 401);
+        $this->assertEquals(api_response('ok')->getStatusCode(), 200);
+
+        $this->assertJsonStringEqualsJsonString(api_response('fail', 400)->getContent(), json_encode(['error' => ['code' => 400, 'msg' => 'fail']]));
+
+        $this->assertJsonStringNotEqualsJsonString(api_response('fail', 400)->getContent(), json_encode('ok'));
     }
 }
