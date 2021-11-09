f5e31b77a7b2a1df4769c83f04c01ef430d4bea1
Andrey Helldar <helldar@ai-rus.com>
2021-11-10 00:18:25 +0300

    Added copyrights

--- src/Parsers/Main.php
+++ src/Parsers/Main.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * This file is part of the "dragon-code/api-response" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/TheDragonCode/api-response
+ */
+
 namespace DragonCode\ApiResponse\Parsers;
 
 class Main extends Parser
